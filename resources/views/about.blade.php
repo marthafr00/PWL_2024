356f5bc8d7d10c8079792c51c5d5417c54461175
marthafr00 <afrhissamartha771@gmail.com>
2024-02-22 14:55:47 +0700

    pertemuan 2_langkah praktikum 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <tittle>ABOUT</tittle>
+        <style>
+            h2{
+                color: pink;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>NIM: 2141762050</h2>
+        <h2>Nama: NADILLA AMANDA MARTHA AFRHISSA</h2>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'NIM: 2141762050, Nama: NADILLA AMANDA MARTHA AFRHISSA';
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
