4ba44a8c34d8e610f4c6e70d2cbc87a92536fbe5
marthafr00 <afrhissamartha771@gmail.com>
2024-02-22 20:38:44 +0700

    pertemuan 2_langkah praktikum 9

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting(){
-        return view('blog.hello', ['name' => 'Andi']);
+        return view('blog.hello')
+            ->with('name', 'Nadilla')
+            ->with('occupation','Programmer');
         }
 }
